692e7be782b146f46e8538107af73746fdeb29f6
Tomasz Kwiatkowski <tomus19@gmail.com>
2018-07-19 01:14:49 +0200

    last polish v2

--- app/src/main/java/com/example/android/csgocounter/MainActivity.java
+++ app/src/main/java/com/example/android/csgocounter/MainActivity.java
@@ -11,8 +11,12 @@ import android.widget.TextView;
 import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
-    int teamAMatchScore, teamBMatchScore = 0;
-    int teamARoundScore, teamBRoundScore = 0;
+    int teamAMatchScore = 0;
+    int teamBMatchScore = 0;
+    int teamARoundScore = 0;
+    int teamBRoundScore = 0;
+    int teamAHeadshots = 0;
+    int teamBHeadshots = 0;
     int format = 0;
     Spinner spinner_ct;
     Spinner spinner_t;
@@ -55,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     /*
      * Methods for buttons.
      */
-    void buttonTeamAPoint(View view) {
+    public void buttonTeamAPoint(View view) {
         if (!isRoundEnded() && !isMatchEnded()) {
             teamARoundScore += 1;
             winner = spinner_ct.getSelectedItem().toString();
@@ -65,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void buttonTeamBPoint(View view) {
+    public void buttonTeamBPoint(View view) {
         if (!isRoundEnded() && !isMatchEnded()) {
             teamBRoundScore += 1;
             winner = spinner_t.getSelectedItem().toString();
@@ -74,26 +78,55 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void teamAHeadshotsIncrease(View view) {
+        teamAHeadshots++;
+
+        displayHeadshots((TextView) findViewById(R.id.team_a_headshots), teamAHeadshots);
+    }
+
+    public void teamAHeadshotsDecrease(View view) {
+        if (teamAHeadshots > 0)
+            teamAHeadshots--;
+
+        displayHeadshots((TextView) findViewById(R.id.team_a_headshots), teamAHeadshots);
+    }
+
+    public void teamBHeadshotsIncrease(View view) {
+        teamBHeadshots++;
+
+        displayHeadshots((TextView) findViewById(R.id.team_b_headshots), teamBHeadshots);
+    }
+
+    public void teamBHeadshotsDecrease(View view) {
+        if (teamBHeadshots > 0)
+            teamBHeadshots--;
+
+        displayHeadshots((TextView) findViewById(R.id.team_b_headshots), teamBHeadshots);
+    }
+
     void reset(View view) {
         teamARoundScore = 0;
         teamBRoundScore = 0;
         teamAMatchScore = 0;
         teamBMatchScore = 0;
+        teamAHeadshots = 0;
+        teamBHeadshots = 0;
         displayMatchScore();
         displayTeamARoundScore(teamARoundScore);
         displayTeamBRoundScore(teamBRoundScore);
+        displayHeadshots((TextView) findViewById(R.id.team_a_headshots), teamAHeadshots);
+        displayHeadshots((TextView) findViewById(R.id.team_b_headshots), teamBHeadshots);
         displayMessage("Match about to start!");
     }
 
-    void roundSummary(View view) {
+    public void roundSummary(View view) {
         if (isRoundEnded()) {
             if (teamARoundScore > teamBRoundScore) {
                 teamAMatchScore += 1;
                 winner = spinner_ct.getSelectedItem().toString();
                 loser = spinner_t.getSelectedItem().toString();
                 afterMap();
-            }
-            else {
+            } else {
                 teamBMatchScore += 1;
                 winner = spinner_t.getSelectedItem().toString();
                 loser = spinner_ct.getSelectedItem().toString();
@@ -105,12 +138,12 @@ public class MainActivity extends AppCompatActivity {
             displayTeamBRoundScore(teamBRoundScore);
             displayMatchScore();
 
-            if(isMatchEnded())
+            if (isMatchEnded())
                 afterMatch();
         }
     }
 
-    void onRadioButtonClick(View view) {
+    public void onRadioButtonClick(View view) {
         boolean checked = ((RadioButton) view).isChecked();
 
         switch (view.getId()) {
@@ -150,36 +183,35 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(score);
     }
 
-    void displayFormat()
-    {
+    void displayFormat() {
         TextView formatView = (TextView) findViewById(R.id.format);
         String formatText = "( BO" + format + " )";
         formatView.setText(formatText);
     }
 
+    void displayHeadshots(TextView view, int number) {
+        view.setText(String.valueOf(number));
+    }
+
     /*
      * Message methods
      */
-    void afterRound()
-    {
+    void afterRound() {
         String message = winner + " has won the round!";
         displayMessage(message);
     }
 
-    void afterMap()
-    {
+    void afterMap() {
         String message = winner + " has won the map!";
         displayMessage(message);
     }
 
-    void afterMatch()
-    {
+    void afterMatch() {
         String message = winner + " has defeted " + loser + "!";
         displayMessage(message);
     }
 
-    void displayMessage(String message)
-    {
+    void displayMessage(String message) {
         TextView messageBox = (TextView) findViewById(R.id.message_box);
         messageBox.setText(message);
     }
